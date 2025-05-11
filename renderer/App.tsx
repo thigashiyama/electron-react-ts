53c817984b0b242e64619542dc455053434eaf64
thigashiyama <tele1oo2@yahoo.co.jp>
2025-05-11 15:10:26 +0900

    upadate

--- renderer/App.tsx
+++ renderer/App.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
-const App: React.FC = () => {
-  return (
-    <div>
-      <h1>Hello from React!</h1>
-    </div>
-  );
+const App = (): JSX.Element => {
+  return <h1>Hello</h1>;
 };
 
 export default App;
